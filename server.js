559a48ac78d3db09b0fda4ef80b5e15d3db1e984
antcoop <iam@anthonycooper.me>
2022-09-27 09:06:17 -0500

    updates to project

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
 
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: process.env.SECRET,
@@ -21,7 +22,6 @@ const sess = {
   }),
 };
 
-// Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -33,5 +33,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
 });
