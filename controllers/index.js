6239911005f42145111238ee77a6794eed4f883b
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-09-29 14:57:56 -0400

    profile page, homepage

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const loginRoutes = require('./loginRoutes');
+const profileRoutes = require('./profileRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/login', loginRoutes);
+router.use('/profile', profileRoutes);
 
 module.exports = router;
