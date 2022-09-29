5a89ff6e218428a5d69a24cc91cd5c3ecda775e9
Jacob Lockhart <procoder165@gmail.com>
2022-09-29 10:23:37 -0400

    JL_commit_08

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {User} = require("../models");
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('homepage');
 });
 
 module.exports = router;
