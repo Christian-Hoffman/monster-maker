e191b8d93b84f6cdf13264918098c070fddc0411
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-09-30 15:36:08 -0400

    connection issue

--- db/sync.js
+++ db/sync.js
@@ -6,7 +6,7 @@ const { DB_NAME } = process.env;
 (async () => {
   try {
     const statement = `CREATE DATABASE IF NOT EXISTS ${DB_NAME}`;
-    const db = await mysql.createConnection(process.env.PORT? `mysql://nocfd75lv5g0utl6:a6ppzo7e49bsavzu@au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/txmnv0ir0a6kke1o` : 'mysql://root@127.0.0.1:3306');
+    const db = await mysql.createConnection('mysql://root@127.0.0.1:3306');
     await db.execute(statement);
     console.log(`${DB_NAME} created`);
     process.exit();
