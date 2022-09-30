0d08ddfea148e201575dbef06b03901315ce292f
Jacob Lockhart <procoder165@gmail.com>
2022-09-30 15:38:37 -0400

    JL_commit_14

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -1,10 +1,34 @@
 const profile = require('express').Router();
-
+const {User, Character} = require("../models");
 
 
 profile.get('/', async (req, res) => {
-  res.render('profile', {isOnline: req.session.isOnline});
+  const data = await User.findOne({
+    where: {
+      isOnline: true
+    },
+    include: [{model: Character, 
+      attributes: [
+                "monster", 
+                "character_type",
+                "description",
+                "weapons",
+                "health",
+                "user_id",
+              ]}, 
+            ]
+  });
+  if(data){
+ 
+const userStats = JSON.parse(JSON.stringify(data));
+console.log("user Stats", userStats);
+  // res.send(userStats);
 
+  res.render("profile", {userStats, isOnline: req.session.isOnline});
+  }
+  else{
+    res.json({message: "Something went wrong"});
+  }
 });
 
 module.exports = profile;
