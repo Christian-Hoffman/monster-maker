a07bbad80a3ddaf06767464dc894968ad48fbfe5
Jacob Lockhart <procoder165@gmail.com>
2022-09-30 12:27:10 -0400

    JL_commit_11

--- controllers/create_monsterRoute.js
+++ controllers/create_monsterRoute.js
@@ -1,6 +1,7 @@
 const create_monster = require('express').Router();
+const auth = require("../utils/auth");
 
-create_monster.get('/', (req, res) => {
+create_monster.get('/', auth, (req, res) => {
   res.render('create_monster', {isOnline: req.session.isOnline});
 });
 
